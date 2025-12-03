60cd1e0007b6241db731d0eb8acf37bbb650e0c8
hereisphil <thereisphil@gmail.com>
2025-12-03 11:13:15 -0600

    Followed Brandon's tutorial 2.4 vid1

--- api/server.js
+++ api/server.js
@@ -2,9 +2,10 @@ const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
 // const path = require("path");
-// const cors = require("cors");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 const PORT = process.env.PORT || 8000;
 
---
+++ reactjs/src/App.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const API_BASE =
+    import.meta.env.NODE_ENV === "development"
+      ? `http://localhost:8000`
+      : import.meta.env.VITE_BASE_URL;
+
+  const getStudents = async () => {
+    setLoading(true);
+    try {
+      const rawStudents = await fetch(`${API_BASE}/students`);
+      console.log("Fetch >>>", rawStudents);
+
+      const allStudents = await rawStudents.json();
+      console.log("Parsed >>>", allStudents);
+
+      setStudents(allStudents);
+      console.log("State >>>", students);
+    } catch (error) {
+      console.log(error.message || "Unexpected Error");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  let ignore = false;
+  useEffect(() => {
+    if (!ignore) {
+      getStudents();
+    }
+  }, []);
+
+  return (
+    <main>
+      <h1>Students:</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {students.map((student, index) => (
+            <li key={index}>{student.name}</li>
+          ))}
+        </ul>
+      )}
+    </main>
+  );
+}
+
+export default App;
