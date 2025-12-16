147376d2185bc27f9ca2427078ddbb23225573d0
hereisphil <thereisphil@gmail.com>
2025-12-16 11:16:46 -0600

    added user auth routes

--- api/server.js
+++ api/server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
-// const path = require("path");
 const cors = require("cors");
 
 const app = express();
@@ -10,6 +9,7 @@ app.use(cors());
 const PORT = process.env.PORT || 8000;
 
 const studentRouter = require("./routes/students");
+const authRouter = require("./routes/auth");
 
 const DATABASE_URL = process.env.DATABASE_URL;
 
@@ -20,6 +20,7 @@ db.once("open", () => console.log("Database Connection Established"));
 
 app.use(express.json());
 app.use("/api/v1/students", studentRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
